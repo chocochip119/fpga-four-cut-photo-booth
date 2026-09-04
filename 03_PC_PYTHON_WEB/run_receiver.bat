21e3fdfe80df86f6edd2cf197cce1580cf526f81
chocochip119 <alfmwldnjs@gmail.com>
2026-09-04 14:57:32 +0900

    fix: run UART receiver at 640x480

--- 03_PC_PYTHON_WEB/run_receiver.bat
+++ 03_PC_PYTHON_WEB/run_receiver.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
 set /p UART_PORT=COM port input (example COM5): 
-python receive_uart_image.py --port %UART_PORT% --baud 1000000
+python receive_uart_image.py --port %UART_PORT% --baud 1000000 --width 640 --height 480
 pause
