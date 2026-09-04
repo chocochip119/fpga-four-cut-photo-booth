a3b7397448c2782c2e28c34cf568e2a479b90b65
chocochip119 <alfmwldnjs@gmail.com>
2026-09-04 16:44:01 +0900

    fix: use configured web port in debug launcher

--- 03_PC_PYTHON_WEB/run_debug.bat
+++ 03_PC_PYTHON_WEB/run_debug.bat
@@ -1,2 +1,12 @@
 @echo off
-start "" "http://127.0.0.1:5000/static/state_debug.html?state=3"
+setlocal
+cd /d "%~dp0"
+
+set "WEB_PORT=%~1"
+if not defined WEB_PORT (
+    for /f %%P in ('powershell -NoProfile -Command "(Get-Content -Raw 'web_config.json' ^| ConvertFrom-Json).web_port" 2^>nul') do set "WEB_PORT=%%P"
+)
+
+if not defined WEB_PORT set "WEB_PORT=5000"
+start "" "http://127.0.0.1:%WEB_PORT%/static/state_debug.html?state=3"
+endlocal
